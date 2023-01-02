d3d77ddbf50f12fdc52164ecdf27c3557e3213c9
Sozokujin <lcorre.pro@gmail.com>
2023-01-02 15:52:27 +0100

    add navbar

---
+++ src/components/organisms/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Bell } from "../atoms/Bell/Bell";
+import { ImageProfil } from "../atoms/ImageProfil/ImageProfil";
+import { Logo } from "../atoms/Logo/Logo";
+import { StyledNavbar } from "./styles";
+
+export const Navbar = () => {
+    return (
+        <StyledNavbar>
+            <Logo></Logo>
+            <Bell></Bell>
+            <ImageProfil></ImageProfil>
+        </StyledNavbar>
+    )
+}
---
+++ src/components/organisms/styles.js
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+
+export const StyledNavbar = styled.div`
+    border : 1px solid blue;
+    display : flex;
+    flex-direction : row;
+    justify-content: space-between;
+`;
