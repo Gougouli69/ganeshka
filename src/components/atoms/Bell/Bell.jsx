089bb755e832ceb4e54760b8668fdfc3e5a2647e
Sozokujin <lcorre.pro@gmail.com>
2023-01-02 15:40:47 +0100

    add: component bell

---
+++ src/components/atoms/Bell/Bell.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledBell } from "./styles";
+import { BiBell } from "react-icons/bi";
+
+export const Bell = () => {
+    return (
+        <StyledBell>
+            <BiBell></BiBell>
+        </StyledBell>
+    )
+}
