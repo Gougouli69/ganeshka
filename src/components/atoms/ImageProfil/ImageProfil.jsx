a88af3fc0b38461ad7eeeffb0bd29f05b12e921b
Sozokujin <lcorre.pro@gmail.com>
2023-01-03 09:53:03 +0100

    remove css inline

--- src/components/atoms/ImageProfil/ImageProfil.jsx
+++ src/components/atoms/ImageProfil/ImageProfil.jsx
@@ -3,16 +3,8 @@ import {StyledImageProfil} from './styles.js';
 
 
 export const ImageProfil = () => {
-
-    const mystyle = {
-        width: "100%",
-        height: "100%",
-        borderRadius: "100%"
-      };
-
     return (
-        <StyledImageProfil>        
-            <img src={process.env.PUBLIC_URL +"/dist/images/logo.jpg"} alt="image" style={mystyle}/>
+        <StyledImageProfil src={process.env.PUBLIC_URL +"/dist/images/logo.jpg"} alt="Photo de profil">
         </StyledImageProfil>
     )
 }
--- src/components/atoms/ImageProfil/styles.js
+++ src/components/atoms/ImageProfil/styles.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 
-export const StyledImageProfil = styled.div`
+export const StyledImageProfil = styled.img`
     border-radius: 100%;
     width: 43px;
     height: 43px;
