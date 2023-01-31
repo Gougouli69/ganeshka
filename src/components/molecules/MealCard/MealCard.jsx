8d943834d1aac28c583ebaa547bee36bb0cc2f2f
Sozokujin <lcorre.pro@gmail.com>
2023-01-31 18:27:51 +0100

    change dishes info delete edit

--- src/components/molecules/MealCard/MealCard.jsx
+++ src/components/molecules/MealCard/MealCard.jsx
@@ -2,74 +2,19 @@ import React from "react";
 import { ImageMeal } from "../../atoms/ImageMeal/ImageMeal";
 import { StyledMealCard } from "./styles";
 import { Text } from "../../atoms/Text/Text";
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
-import CancelIcon from '@mui/icons-material/Cancel';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button'
-import { useState } from "react";
-import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 
 
 export const MealCard = ({value}) => {
     const navigate = useNavigate(); 
-
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    const handleCloseDelete = () => {
-        let config = {
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        };
-
-        axios.delete(
-            'https://urzggw0p.directus.app/items/Meal/'+value.id, 
-            config
-        ).then(
-            (response) => {
-            }
-        ).finally(() =>{
-            setOpen(false);
-            navigate('/');
-        });
-    }
     return(
         <div>
-        <StyledMealCard>
+        <StyledMealCard onClick={() => navigate('/info-dishes/'+value.id)}>
             <ImageMeal src={value.picture}></ImageMeal>
             <Text margin={0}>{value.name}</Text>
-            <CancelIcon onClick={handleClickOpen}></CancelIcon>
-            <ModeEditIcon></ModeEditIcon>
+
         </StyledMealCard>
-
-
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-        >
-            <DialogTitle id="alert-dialog-title">
-                {"Voulez-vous vraiment supprimer ce plat ?"}
-            </DialogTitle>
-            <DialogActions>
-                <Button onClick={handleClose}>Annnuler</Button>
-                <Button color='error' onClick={handleCloseDelete} autoFocus>
-                    Supprimer
-                </Button>
-            </DialogActions>
-        </Dialog>
         </div>
     )
 }
